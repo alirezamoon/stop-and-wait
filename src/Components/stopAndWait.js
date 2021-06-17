add11ddc02180769c2de46af100be4635270458e
Alireza <real7alireza@gmail.com>
2021-06-17 23:52:07 +0430

    change some styles

--- src/Components/stopAndWait.js
+++ src/Components/stopAndWait.js
@@ -6,25 +6,27 @@ import { NavLink } from "react-router-dom";
 const StopAndWait = () => {
   return (
     <Container>
-      <Row className="text-light h1 py-2 m-0 bg-info mt-5">
-        <Col>** Stop And Wait **</Col>
+      <Row className=" text-secondary h3 py-2 m-0 mt-5">
+        <Col xs={12}>تمرین پیاده سازی</Col>
+        <Col xs={12}>درس انتقال داد ها - دکتر عابدی</Col>
+        <Col xs={12} className='rounded text-dark py-2 mt-5 bg-info'>** Stop And Wait **</Col>
       </Row>
       <Row xs={5} className="mt-5 justify-content-center">
         <NavLink to="/error-free">
-          <Button className="w-100">Error Free</Button>
+          <Button variant='warning' className="w-100">Error Free</Button>
         </NavLink>
       </Row>
       <Row xs={5} className="mt-5 justify-content-center">
         <NavLink to="/lost-frame">
-          <Button className="w-100">Lost Frame</Button>
+          <Button variant='warning' className="w-100">Lost Frame</Button>
         </NavLink>
       </Row>
       <Row xs={5} className="mt-5 justify-content-center">
         <NavLink to="/lost-ack">
-          <Button className="w-100">Lost Ack</Button>
+          <Button variant='warning' className="w-100">Lost Ack</Button>
         </NavLink>
       </Row>
-      <Row className="text-light h1 py-2 mt-5 m-0 bg-info">
+      <Row className="rounded text-dark h3 py-2 mt-5 m-0 bg-info">
         <Col>Alireza Khanamani 96405036</Col>
       </Row>
     </Container>
