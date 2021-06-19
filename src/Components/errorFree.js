748e9e0b8e7c0741e65178d5c90b57096098615e
Alireza <real7alireza@gmail.com>
2021-06-19 08:40:13 +0430

    finall

--- src/Components/errorFree.js
+++ src/Components/errorFree.js
@@ -8,31 +8,35 @@ const ErrorFree = () => {
 
   const [start, setStart] = useState(false);
 
-  const [frames, setFrames] = useState( [
-    ["1","bg-light"],
-    ["2","bg-light"],
-    ["3","bg-light"],
-    ["4","bg-light"],
-    ["5","bg-light"],
+  const [frames, setFrames] = useState([
+    ["1", "bg-light"],
+    ["2", "bg-light"],
+    ["3", "bg-light"],
+    ["4", "bg-light"],
+    ["5", "bg-light"],
   ]);
 
   const timeDev = () => {
     let time = 0;
-    let newFrames = [...frames]
+    let newFrames = [...frames];
     newFrames.map((frame) => {
       setTimeout(() => {
-        frame[1]= 'bg-warning'
-        setFrames(newFrames)
+        frame[1] = "bg-warning";
+        setFrames(newFrames);
         setMsgSender((p) => [...p, `frame ${frame[0]} sent`]);
       }, (time += 2000));
       setTimeout(() => {
         setReciever((p) => [...p, frame[0]]);
-        setMsgReciever((p) => [...p, `frame ${frame[0]} recieved`, `ack ${1+(+frame[0])} sent`]);
+        setMsgReciever((p) => [
+          ...p,
+          `frame ${frame[0]} recieved`,
+          `ack ${1 + +frame[0]} sent`,
+        ]);
       }, (time += 2000));
       setTimeout(() => {
-        frame[1]= 'bg-success'
-        setFrames(newFrames)
-        setMsgSender((p) => [...p, `ack ${1+(+frame[0])} received`]);
+        frame[1] = "bg-success";
+        setFrames(newFrames);
+        setMsgSender((p) => [...p, `ack ${1 + +frame[0]} received`]);
       }, (time += 2000));
     });
   };
@@ -45,7 +49,7 @@ const ErrorFree = () => {
 
   return (
     <>
-          <Row className="text-light h3 py-1 m-0 bg-info">
+      <Row className="text-light h3 py-1 m-0 bg-info">
         <Col>Error Free</Col>
       </Row>
       <Container>
@@ -72,7 +76,9 @@ const ErrorFree = () => {
           <Col className="d-flex flex-column justify-content-center mx-5">
             {frames.map((frame) => {
               return (
-                <Row className={`${frame[1]} mb-2 mx-5 justify-content-center text-center py-2 rounded`}>
+                <Row
+                  className={`${frame[1]} mb-2 mx-5 justify-content-center text-center py-2 rounded`}
+                >
                   <Col>frame {frame[0]}</Col>
                 </Row>
               );
